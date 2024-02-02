74f61f23b62da3f80a907c6ff5960835e5c6210f
autonomuscoder <mahiabirgit@tutanota.com>
2024-02-02 19:11:48 +0600

    added gsap animations

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function toggleMenu() {
-	var navLinks = document.getElementById("nav-links");
-	navLinks.classList.toggle("active");
-}
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('.message-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
+gsap.from('#hero-img', {duration: 1, scale: '0.5', ease: 'circ-out'});
+gsap.from('.about', {duration: 2, scale: '0.5'})
+gsap.to('.about', {scrollTrigger: '.about', duration: 2, scale: '1'})
+gsap.from('.card', {duration: 2, scale: '0.5', ease: 'circ-out'})
+gsap.to('.card', {scrollTrigger: '.card', duration: 2, scale: '1', ease: 'circ-out'})
+gsap.from('.contact-card', {duration: 2, scale: '0.5', ease: 'circ-out'})
+gsap.to('.contact-card', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
+gsap.from('.email', {duration: 2, scale: '0.5', ease: 'circ-out'})
+gsap.to('.email', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
+gsap.from('.title-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
