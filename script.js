6426a82515e1712cda876871518b46f245465acf
autonomuscoder <mahiabirgit@tutanota.com>
2024-02-14 21:20:43 +0600

    changed navbar style

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.from('.message-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
 gsap.from('#hero-img', {duration: 1, scale: '0.5', ease: 'circ-out'});
-gsap.from('.about', {duration: 2, scale: '0.5'})
+// gsap.from('.about', {duration: 2, scale: '0.5'})
 gsap.to('.about', {scrollTrigger: '.about', duration: 2, scale: '1'})
-gsap.from('.card', {duration: 1, scale: '0.3', ease: 'circ-out'})
-gsap.to('.card', {scrollTrigger: '.card', duration: 1, scale: '1', ease: 'circ-out'})
+gsap.from('.card', {scrollTrigger: '.card', duration: 1, y: '100%', ease: 'circ-out'})
 gsap.from('.contact-card', {duration: 2, scale: '0.5', ease: 'circ-out'})
 gsap.to('.contact-card', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
 gsap.from('.email', {duration: 1, scale: '0.3', ease: 'circ-out'})
