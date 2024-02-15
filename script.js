c0575c55a27d93a134cfc1f90c567cad0b306e12
autonomuscoder <mahiabirgit@tutanota.com>
2024-02-15 14:40:17 +0600

    minor bug fix

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.from('.message-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
 gsap.from('#hero-img', {duration: 1, scale: '0.5', ease: 'circ-out'});
-// gsap.from('.about', {duration: 2, scale: '0.5'})
-gsap.to('.about', {scrollTrigger: '.about', duration: 2, scale: '1'})
+gsap.from('.about', {scrollTrigger: '.about', duration: 1, y: '100%'})
 gsap.from('.card', {scrollTrigger: '.card', duration: 1, y: '100%', ease: 'circ-out'})
 gsap.from('.contact-card', {duration: 2, scale: '0.5', ease: 'circ-out'})
 gsap.to('.contact-card', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
