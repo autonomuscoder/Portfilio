03c7ebf3e96f05ad84c3433566147f40706cbd5a
xero <112854891+autonomuscoder@users.noreply.github.com>
2024-02-18 11:15:55 +0600

    bug fix

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from('.message-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
 gsap.from('#hero-img', {duration: 1, scale: '0.5', ease: 'circ-out'});
 gsap.from('.about', {scrollTrigger: '.about', duration: 1, x: '-100%'})
-gsap.from('.card-wrapper', {scrollTrigger: '.card', duration: 1, y: '100%', ease: 'circ-out'})
+gsap.from('.mob-about', {scrollTrigger: '.mob-about', duration: 1, x: '-100%'})
+gsap.from('.skill-wrapper', {scrollTrigger: '.skill-wrapper', duration: 1, x: '-100%', ease: 'circ-out'})
 gsap.from('.contact-card', {duration: 2, scale: '0.5', ease: 'circ-out'})
 gsap.to('.contact-card', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
 gsap.from('.email', {duration: 1, scale: '0.3', ease: 'circ-out'})
