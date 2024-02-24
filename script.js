1abe642195308ef205a250f2bf767f0903329afe
autonomuscoder <mahiabirgit@tutanota.com>
2024-02-24 18:00:28 +0600

    added preloader

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.from('.message-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
-gsap.from('#hero-img', {duration: 1, scale: '0.5', ease: 'circ-out'});
-gsap.from('.about', {scrollTrigger: '.about', duration: 1, x: '-100%'})
-gsap.from('.mob-about', {scrollTrigger: '.mob-about', duration: 1, x: '-100%'})
-gsap.from('.skill-wrapper', {scrollTrigger: '.skill-wrapper', duration: 1, x: '-100%', ease: 'circ-out'})
-gsap.from('.contact-card', {duration: 2, scale: '0.5', ease: 'circ-out'})
-gsap.to('.contact-card', {scrollTrigger: '.contact-card', duration: 2, scale: '1', ease: 'circ-out'})
-gsap.from('.email', {duration: 1, scale: '0.3', ease: 'circ-out'})
-gsap.to('.email', {scrollTrigger: '.contact-card', duration: 1, scale: '1', ease: 'circ-out'})
-gsap.from('.title-wrapper', {duration: 1, y: '-100%', ease: 'bounce'});
+
+gsap.from(".txt-gradient", { duration: 1, x: "-100%", ease: "back-out" });
+gsap.from("#hero-img", { duration: 1, scale: "0.5", ease: "circ-out" });
+gsap.from(".about", { scrollTrigger: ".about", duration: 1, x: "-100%" });
+gsap.from(".mob-about", {
+  scrollTrigger: ".mob-about",
+  duration: 1,
+  x: "-100%",
+});
+gsap.from(".skill-wrapper", {
+  scrollTrigger: ".skill-wrapper",
+  duration: 1,
+  x: "-100%",
+  ease: "circ-out",
+});
+gsap.from(".contact-card", { duration: 2, scale: "0.5", ease: "circ-out" });
+gsap.to(".contact-card", {
+  scrollTrigger: ".contact-card",
+  duration: 2,
+  scale: "1",
+  ease: "circ-out",
+});
+gsap.from(".email", { duration: 1, scale: "0.3", ease: "circ-out" });
+gsap.to(".email", {
+  scrollTrigger: ".contact-card",
+  duration: 1,
+  scale: "1",
+  ease: "circ-out",
+});
+gsap.from(".title-wrapper", { duration: 1, y: "-100%", ease: "bounce" });
+
+var loader = document.querySelector(".loader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
