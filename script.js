6a4c56c7a4c44b9c802fe1b9e3f50b6334742e48
autonomuscoder <mahiabirgit@tutanota.com>
2024-04-13 16:27:02 +0600

    better scroll animations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const loader = document.querySelector(".loader");
-window.addEventListener("load", function() {
-	loader.style.display = "none";
+window.addEventListener("load", function () {
+  loader.style.display = "none";
 });
 
 const navBtn = document.querySelector(".nav-btn");
@@ -8,22 +8,22 @@ const closeBtn = document.querySelector(".close-btn");
 const sideBar = document.querySelector(".side-nav");
 
 navBtn.addEventListener("click", () => {
-	sideBar.classList.toggle("reveal");
+  sideBar.classList.toggle("reveal");
 });
 
 closeBtn.addEventListener("click", () => {
-	sideBar.classList.remove("reveal");
+  sideBar.classList.remove("reveal");
 });
 
-ScrollReveal({ reset: true, distance: "50px", duration: 2000 }).reveal(
-	".name, .title, .card",
-	{ delay: 300, origin: "bottom" },
+ScrollReveal({ reset: true, distance: "70px", duration: 2000 }).reveal(
+  ".name, .title, .card, .post-heading, .contact-wrapper, .pulse-wrapper",
+  { delay: 300, origin: "bottom" },
 );
-ScrollReveal({ reset: true, distance: "50px", duration: 2000 }).reveal(
-	".about-title",
-	{ delay: 300, origin: "left" },
+ScrollReveal({ reset: true, distance: "150px", duration: 2000 }).reveal(
+  ".about-title, #hero-img",
+  { delay: 300, origin: "left" },
 );
-ScrollReveal({ reset: true, distance: "50px", duration: 2000 }).reveal(
-	".about-text",
-	{ delay: 400, origin: "right" },
+ScrollReveal({ reset: true, distance: "150px", duration: 2000 }).reveal(
+  ".about-text, .post-card1,.post-card2,.post-card3,.post-card4",
+  { delay: 400, origin: "right" },
 );
